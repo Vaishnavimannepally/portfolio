fd24b83e68c5a16eed42c518b4e6ba383210bbb9
RegistrationForm <vaishnavimannepally@gmail.com>
2024-07-16 09:39:56 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,182 @@
+const logotext = "VAISHNAVI";
+const meta = {
+    title: "VAISHNAVI MANNEPALLY",
+    description: "",
+};
+
+const introdata = {
+    title: "I’m VAISHNAVI MANNEPALLY",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I love problem solving",
+    },
+    description: "Highly skilled in problem-solving and design, proficient in Java, SQL, HTML and CSS. Seeking opportunities to apply technical expertise and translate business needs into innovative solutions as an entry-level software engineer in a reputable technology-focused organization.",
+    your_img_url: "",
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme: "My name is Vaishnavi. I live Hyderabad, Telangana. I am pursuing my B-Tech in Information Technology from MLR Institute of Technology, Hyderabad. My areas of interest are Web Development, Problem Solving . I am currently studying Python to improve my coding skills as well. For Web Development, I have learnt HTML, CSS, Javascript and some frameworks like Bootstrap. I have also completed virtual internship certification courses Google AI-ML program, Cisco Networking  and  Python.",
+};
+const worktimeline = [
+    {
+        jobtitle: "DataBase Management System(Elite)",
+        where: "NPTEL",
+        date: "2024",
+    },
+
+    {
+        jobtitle: "Programming In JAVA(Elite)",
+        where: "NPTEL",
+        date: "Oct 2023",
+    },
+    {
+        jobtitle: "AWS Cloud Practitioner Essentials",
+        where: "AWS training and certification",
+        date: "Dec 2023",
+    },
+    
+];
+
+const Education = [
+    {
+        jobtitle: "B.Tech(Information Technology)",
+        where: "MLR INSTITUTE OF TECHNOLOGY",
+        CGPA: "9.23/10",
+        date: "2025",
+    },
+
+    {
+        jobtitle: "Board of Intermediate Education ",
+        where: "Narayana Junior College ",
+        CGPA: "9.5/10",
+        date: "2021",
+    },
+    {
+        jobtitle: "CBSE (CLASS X) ",
+        where: "HAL secondary school",
+        CGPA: "82.6%",
+        date: "2019",
+    },
+    
+];
+
+const skills = [{
+        name: "Java",
+        value: 90,
+    },
+    {
+        name: "DBMS",
+        value: 85,
+    },
+    {
+        name: "HTML/css/js",
+        value: 85,
+    },
+    {
+        name: "Python",
+        value: 70,
+    },
+    {
+        name: "MERN",
+        value: 80,
+    },
+];
+
+const services = [{
+        title: "WorinWell(Java Intern)",
+        description: "Actively participated in developing a dynamic website using Java Servlets and JDBC and this experience enabled me to gain comprehensive knowledge and practical skills in both Java Servlets and JDBC"
+    },
+    {
+        title: "AI-ML(Google)",
+        description: "",
+    },
+   
+];
+
+
+const dataportfolio = [{
+        img: "https://media.licdn.com/dms/image/C5612AQHXpElnKlgFTA/article-cover_image-shrink_600_2000/0/1631271495780?e=2147483647&v=beta&t=G-v2giaPyk2MRBprIyIvJ5voiKmuq89bEH9F-MhPYak",
+        description: "Hsdpital Management System",
+        link: "https://vaishnavimannepally.github.io/Hospital_Management_System/",
+    },
+    {
+        img: "https://media.licdn.com/dms/image/D5612AQGnkBqtztLtEA/article-cover_image-shrink_720_1280/0/1693731593756?e=2147483647&v=beta&t=2xK3qs5PJ5A2ixs08XC9N5nU49pRIVSTg3-HYQZLJt4",
+    description: "Employee Management System",
+    link: "",
+    },
+    {
+        img: "",
+        description: "",
+        link: "",
+    },
+    {
+        img: "",
+    description: "",
+    link: "",
+    },
+    
+    {
+        img: "https://m.economictimes.com/thumb/msid-87888935,width-1200,height-900,resizemode-4,imgsize-41278/e-commerce-getty.jpg",
+    description: "Shoppie",
+    link: "",
+    },
+
+    {
+        img: "https://www.shutterstock.com/image-vector/chat-bot-logo-design-concept-600nw-1938811039.jpg",
+        description: "OpenAI Chatbot",
+        link: " https://vaishnavimannepally.github.io/OpenAI-Chatbot/",
+    },
+    {
+        img: "",
+        description: "",
+        link: "",
+    },
+    {
+        img: "",
+        description: "",
+        link: "",
+    },
+    {
+        img: "https://www.nicepng.com/png/detail/111-1112693_portfolio-my-portfolio.png",
+    description: "MYPortfolio",
+    link: "",
+    },
+    {
+        img: "https://img.etimg.com/thumb/width-1200,height-900,imgsize-62458,resizemode-75,msid-49086682/magazines/panache/heres-all-you-need-to-know-about-audiobooks.jpg",
+        description: "AudioBook",
+        link: "#",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "vaishnavimannepally@gmail.com",
+    YOUR_FONE: "9392154079",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_e2yz0vg",
+    YOUR_TEMPLATE_ID: "template_hs48ais",
+    publicKey: "JpjHGzUXz0oD6Q0KV",
+};
+
+const socialprofils = {
+    github: "https://github.com/Vaishnavimannepally",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/vaishnavi-mannepally-aba1962b6/",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    Education,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
